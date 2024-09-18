d648314753d064faa0d06fdba4402e0746d5009b
Shanu-Ch-Ghosh <shanughosh236@gmail.com>
2024-09-18 17:36:49 +0530

    jdh

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import { v4 as uuidv4 } from 'uuid';
 
-const App = () => {
+const App = ()  => {
+
+  
+  const [text,setText]=useState("")
+  const [todos,setTodos]=useState([])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <input type="text" placeholder="Enter Your Todo" onChange={(e)=>{
+        setText(e.target.value)
+       }}/>
+       <button onClick={()=>{
+        let newTodo={
+          task:text,
+          id: uuidv4()
+        }
+
+        setTodos([...todos,newTodo])
+
+        setText("")
+
+       }}>Add Todo</button>
+
+       <ul>
+        {todos && todos.map((item)=>{
+          return <li style={{listStyle:"none"}} >{item.task}  <button onClick={()=>{
+            const newArr= todos.filter((todo)=>{
+              return todo.id != item.id
+            })
+
+
+            setTodos(newArr)
+
+          }}>Delete</button></li>
+        })}
+       </ul>
     </div>
   )
 }
